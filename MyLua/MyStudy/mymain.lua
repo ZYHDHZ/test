2e43c542e1fab3e32d740eb3e80553002a0bb202
ZhongYuHan <313968445@qq.com>
2017-01-04 19:11:46 +0800

    mymain.lua

--- MyLua/MyStudy/mymain.lua
+++ MyLua/MyStudy/mymain.lua
@@ -13,12 +13,15 @@ end
 Accout={}
 function Accout:HaHa()
 	print(self.name)
-	print("hahhaa")
+	print("hahhaahahahha")
 end
 -- 这里又加了一句注释
-
+print("shfkjdskfjdsklfsdf")
 NamedAccout=createClass(Named,Accout)
 NamedObject=NamedAccout:new({name="zhongyuhan"})
 
 NamedObject:HaHa()
+
 -- 这里加了一句注释
+
+print("dksfhkdshafjkhsdfsdf")
